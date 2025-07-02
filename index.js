ed46984dfd852fbbb1a9f719e75c06c575057071
Gokul <gp9087@srmist.edu.in>
2025-07-02 23:17:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Clarity Extended</title>
+        <title>Therapist </title>
       </Head>
       <Hero />
       <Features />
